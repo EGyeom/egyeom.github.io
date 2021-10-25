b6df1a3228c5926dfd91c7b185605b1934a26eb0
DESKTOP-FS1C20R\sjsch <ug4306@gmail.com>
2021-10-26 00:13:04 +0900

    token-hide

--- code/token/encryToken.py
+++ code/token/encryToken.py
@@ -1,1 +1,16 @@
-test
+import os
+
+cur_path = os.path.dirname(os.path.abspath(__file__)) # 현재 파일의 절대 경로의 폴더 경로를 가져온다.
+
+# with open(cur_path+"/enToken.txt", 'w+') as f:
+#     for i in token:
+#         f.write(str(ord(i))+ '\n') # 문자 -> unicode 숫자로 변환
+
+token = ''
+with open(cur_path+"/enToken.txt", 'r+') as f:
+    lines = f.readlines()
+    for line in lines:
+        line = line.strip() 
+        token = token + chr(int(line))
+
+print(token)
